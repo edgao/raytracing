9dd38dbc0e1b5f334380c8ee6bfec6f2b7b65626
Jing-Lun_Edward_Gao <edward.jl.gao@berkeley.edu>
2015-03-11 22:49:58 -0700

    Modified classes to use Eigen library; transformations of vectors/normals should work now.

--- src/Class.cpp
+++ src/Class.cpp
@@ -1,207 +1,202 @@
-class Vector {
-public:
-  float x, y, z;
-  Vector(float x, float y, float z) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-  }
-  Vector add(Vector b) { return new Vector(x + b.x, y + b.y, z + b.z); }
-  Vector sub(Vector b) { return new Vector(x - b.x, y - b.y, z - b.z); }
-  Vector scale(float s) { return new Vector(x * s, y * s, z * s); }
-  Vector normalize() { return scale(1 / magnitude()); }
-  float magnitude() { return sqrtf(x * x + y * y + z * z); }
-  // Returns a Vector pointing from a to b
-  static pointToPoint(Point a, Point b) { return new Vector(b.x - a.x, b.y - a.y, b.z - a.z); }
-}
+#include <eigen3/Dense>
 
-class Normal : public Vector {
-public:
-  x, y, z;
-  Normal(float x, float y, float z) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-    normalizeThis();
-  }
-  Normal add(Vector b) { return new Normal(x + b.x, y + b.y, z + b.z); }
-  Normal sub(Vector b) { return new Normal(x - b.x, y - b.y, z - b.z); }  
-private:
-  void normalizeThis() {
-    float mag = magnitude();
-    // because floats are dumb
-    if (mag <= 0.00001) return;
-    x /= mag;
-    y /= mag;
-    z /= mag;
-  }
-}
-
-class Point {
-public:
-  float x, y, z;
-  Point(float x, float y, float z) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-  }
-  Point add(Vector v) { return new Point(x + v.x, y + v.y, z + v.z); }
-  Point sub(Vector v) { return new Point(x - v.x, y - v.y, z - v.z); }
-}
+using namespace Eigen;
 
 class Ray {
 public:
-  Point pos;
-  Vector dir;
+  Vector3f pos;
+  Vector3f dir;
   float t_min, t_max;
-  Ray(Point p, Vector d, float t_min, float t_max) {
+  Ray(Vector3f p, Vector3f d, float t_min, float t_max) {
     pos = p;
     dir = d;
-    this.t_min = t_min;
-    this.t_max = t_max;
+    this->t_min = t_min;
+    this->t_max = t_max;
   }
-}
+  Vector3f evaluate(float t) {
+    Vector3f ret;
+    ret << pos(0) + t * dir(0), pos(1) + t * dir(1), pos(2) + t * dir(2);
+    return ret;
+  }
+};
 
-class Matrix {
+class LocalGeo {
 public:
-  float mat[4][4];
-  Matrix multiply(Matrix o) {
-    Matrix ret = new Matrix();
-    for (int r = 0; r < 4; r++) {
-      for (int c = 0; c < 4; c++) {
-	float sum = 0F;
-	for (int i = 0; i < 4; i++) {
-	  sum += mat[r + i] * o.mat[c + i];
-	}
-	ret.mat[r][c] = sum;
-      }
-    }
+  Vector3f pos;
+  Vector3f normal;
+  LocalGeo(Vector3f p , Vector3f n) {
+    pos = p;
+    normal = n;
   }
-  Matrix invert() {
-    // TODO Implement matrix inversion
-    return NULL;
+};
+
+class MatrixUtils {
+public:
+  static Matrix4f createTranslationMatrix(float tx, float ty, float tz) {
+    Matrix4f ret;
+    ret <<
+      1, 0, 0, tx,
+      0, 1, 0, ty,
+      0, 0, 1, tz,
+      0, 0, 0, 1;
+    return ret;
   }
-  static createRotationMatrix(Vector axis, float radians) {
-    // TODO Implement exponential map rotation
-    return NULL;
+  static Matrix4f createScalingMatrix(float sx, float sy, float sz) {
+    Matrix4f ret;
+    ret <<
+      sx, 0, 0, 0,
+      0, sy, 0, 0,
+      0, 0, sz, 0,
+      0, 0, 0, 1;
+    return ret;
   }
-  static createTranslationMatrix(float tx, float ty, float tz) {
-    Matrix ret = new Matrix();
-    ret.mat = {{1, 0, 0, tx},
-	       {0, 1, 0, ty},
-	       {0, 0, 1, tz},
-	       {0, 0, 0, 1}};
+  static Matrix4f buildRotationMatrix(float rx, float ry, float rz) {
+    Matrix4f x, y, z;
+    Matrix4f m;
+    x <<
+      1, 0, 0, 0,
+      0, cos(rx), -sin(rx), 0,
+      0, sin(rx), cos(rx), 0,
+      0, 0, 0, 1;
+    y <<
+      cos(ry), 0, sin(ry), 0,
+      0, 1, 0, 0,
+      -sin(ry), 0, cos(ry), 0,
+      0, 0, 0, 1;
+    z <<
+      cos(rz), -sin(rz), 0, 0,
+      sin(rz), cos(rz), 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1;
+    m = x * y * z;
+    return m;
   }
-  static createScalingMatrix(float sx, float sy, float sz) {
-    Matrix ret = new Matrix();
-    ret.mat = {{sx, 0, 0, 0},
-	       {0, sy, 0, 0},
-	       {0, 0, sz, 0},
-	       {0, 0, 0, 1}};
+  static Matrix4f buildTranslationMatrix(float tx, float ty, float tz) {
+    Matrix4f ret;
+    ret <<
+      1, 0, 0, tx,
+      0, 1, 0, ty,
+      0, 0, 1, tz,
+      0, 0, 0, 1;
+    return ret;
   }
-}
+  static Matrix4f buildScaleMatrix(float sx, float sy, float sz) {
+    Matrix4f ret;
+    ret <<
+      sx, 0, 0, 0,
+      0, sy, 0, 0,
+      0, 0, sz, 0,
+      0, 0, 0, 1;
+    return ret;
+  }
+};
 
 class Transformation {
 public:
-  Matrix m, minv;
-  Vector transform(Vector v) {
-    float x, y, z;
-    x = v.x * m.mat[0][0] * v.y * mat[0][1] + v.z * mat[0][2] + mat[0][3];
-    y = v.x * m.mat[1][0] * v.y * mat[1][1] + v.z * mat[1][2] + mat[1][3];
-    z = v.x * m.mat[2][0] * v.y * mat[2][1] + v.z * mat[2][2] + mat[2][3];
-    float bot = v.x * m.mat[3][0] * v.y * mat[3][1] + v.z * mat[3][2] + mat[3][3];
-    x /= bot;
-    y /= bot;
-    z /= bot;
-    return new Vector(x, y, z);
+  Matrix4f m, minvt;
+  int type;
+  Transformation(Matrix4f m) {
+    this->m = m;
+    this->minvt = m.inverse().transpose();
   }
-  Point transform(Point v) {
-    float x, y, z;
-    x = v.x * m.mat[0][0] * v.y * mat[0][1] + v.z * mat[0][2] + mat[0][3];
-    y = v.x * m.mat[1][0] * v.y * mat[1][1] + v.z * mat[1][2] + mat[1][3];
-    z = v.x * m.mat[2][0] * v.y * mat[2][1] + v.z * mat[2][2] + mat[2][3];
-    float bot = v.x * m.mat[3][0] * v.y * mat[3][1] + v.z * mat[3][2] + mat[3][3];
-    x /= bot;
-    y /= bot;
-    z /= bot;
-    return new Point(x, y, z);
+  Vector3f transformPoint(Vector3f vec) {
+    Vector4f temp;
+    temp << vec(0), vec(1), vec(2), 1;
+    temp = m * temp;
+    Vector3f ret;
+    ret << temp(0) / temp(3), temp(1) / temp(3), temp(2) / temp(3);
+    return ret;
   }
-  Normal transform(Normal v) {
-    float x, y, z;
-    x = v.x * m.mat[0][0] * v.y * mat[0][1] + v.z * mat[0][2];
-    y = v.x * m.mat[1][0] * v.y * mat[1][1] + v.z * mat[1][2];
-    z = v.x * m.mat[2][0] * v.y * mat[2][1] + v.z * mat[2][2];
-    float bot = v.x * m.mat[3][0] * v.y * mat[3][1] + v.z * mat[3][2];
-    x /= bot;
-    y /= bot;
-    z /= bot;
-    return new Normal(x, y, z);
+  Vector3f transformNormal(Vector3f vec) {
+    Vector4f temp;
+    temp << vec(0), vec(1), vec(2), 0;
+    temp = minvt * temp;
+    Vector3f ret;
+    ret << temp(0), temp(1), temp(2);
+    ret.normalize();
+    return ret;
   }
-  Ray transform(Ray r) {
-    return new Ray(transform(r.pos), transform(r.dir));
+  Ray transformRay(Ray ray) {
+    Ray ret(transformPoint(ray.pos), transformPoint(ray.dir), ray.t_min, ray.t_max);
+    return ret;
   }
-  LocalGeo transform(LocalGeo l) {
-    return new LocalGeo(transform(l.pos), transform(l.normal));
+  LocalGeo transformLocalGeo(LocalGeo geo) {
+    LocalGeo ret(transformPoint(geo.pos), transformNormal(geo.normal));
+    return ret;
   }
-}
+};
 
 class Color {
 public:
   float r, g, b;
   Color(float r, float g, float b) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
+    this->r = r;
+    this->g = g;
+    this->b = b;
   }
-  Color add(Color o) { return new Color(r + o.r, g + o.g, b + o.b); }
-  Color sub(Color o) { return new Color(r - o.r, g - o.g, b - o.b); }
-  Color scale(float s) { return new Color(r * s, g * s, b * s); }
-}
+  Color* add(Color* o) { return new Color(r + o->r, g + o->g, b + o->b); }
+  Color* sub(Color* o) { return new Color(r - o->r, g - o->g, b - o->b); }
+  Color* scale(float s) { return new Color(r * s, g * s, b * s); }
+};
 
 class BRDF {
 public:
   Color kd, ks, ka, kr;
-  BRDF(Color kd, Color ks, Color ka, Color kr) {
-    this.kd = kd;
-    this.ks = ks;
-    this.ka = ka;
-    this.kr = kr;
-  }
-}
+};
 
 class Sample {
 public:
   float x, y;
-  Sample(float x, float y) {
-    this.x = x;
-    this.y = y;
-  }
-}
-
-class LocalGeo {
-public:
-  Point pos;
-  Normal normal;
-  LocalGeo(Point p, Normal n) {
-    pos = p;
-    normal = n;
-  }
-}
+};
 
 class Shape {
-// TODO Implement Triangle and Shape, probably as subclasses of Shape?
 public:
-  bool intersect(Ray& ray, float* thit, LocalGeo* local) {
-    // TODO Check intersection for triangle and sphere
-    return false;
+  virtual bool intersect(Ray&, float*, LocalGeo*);
+  bool intersectP(Ray& ray) {
+    float* thit;
+    LocalGeo* geo;
+    return intersect(ray, thit, geo);
   }
-  bool intersect(Ray& ray) {
-    // TODO blah
-    return false;
-  }
-}
+};
 
-//class Primitive {
-//public:
-//  bool intersect(Ray& ray, float* thit, Intersection* 
-//}
+class Triangle : Shape {
+public:
+  Vector3f p1, p2, p3;
+  bool intersect(Ray& ray, float* thit, LocalGeo* geo) {
+    // TODO Test ray/triangle intersection
+    return false;
+  }
+};
+
+class Sphere : Shape {
+public:
+  Vector3f center;
+  float radius;
+  bool intersect(Ray& ray, float* thit, LocalGeo* geo) {
+    float a, b, c;
+    a = ray.dir(0)*ray.dir(0) + ray.dir(1)*ray.dir(1) + ray.dir(2)*ray.dir(2);
+    b = 2 * (ray.pos(0) - center(0) + ray.pos(1) - center(1) + ray.pos(2) - center(2));
+    c = (ray.pos(0) - center(0)) * (ray.pos(0) - center(0)) + (ray.pos(1) - center(1)) * (ray.pos(1) - center(1)) + (ray.pos(2) - center(2)) * (ray.pos(2) - center(2));
+    float t1, t2;
+    t1 = (-b - sqrtf(b*b - 4 * a * c)) / (2 * a);
+    t2 = (-b + sqrtf(b*b - 4 * a * c)) / (2 * a);
+    if (isnan(t1)) {
+      thit = NULL;
+      geo = NULL;
+      return false;
+    }
+    if (ray.t_min <= t1 && t1 <= ray.t_max) {
+      *thit = t1;
+      // TODO Calculate normal vector at point of intersection
+      return true;
+    }
+    if (ray.t_min <= t1 && t1 <= ray.t_max) {
+      *thit = t1;
+      // TODO Calculate normal vector at point of intersection
+      return true;
+    }
+    thit = NULL;
+    geo = NULL;
+    return false;
+  }
+};
