8f4965fd972ee7cced4481c502835491427d2f42
vincent <vincehayashi@yahoo.com>
2015-03-18 17:58:40 -0700

    unimplemented cases

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,18 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 #include <eigen3/Dense>
+#define cimg_use_jpeg
+#include "CImg.h"
+#include "Class.h"
 
 using namespace std;
 using namespace Eigen;
+using namespace cimg_library;
 
+Transformation trans;
+BRDF brdf;
 enum string_code{
     cam, sph, tri, obj, ltp, ltd, lta, mat, xft, xfr, xfs, dud
 };
@@ -26,31 +33,51 @@ string_code hash_it(string const& op){
     return dud; // literally a dud case, or unimplemented case
 } 
 
-void get_params(string line, int params[]){
-    // takes a string and int[], fills int array with params
+void get_params(string line, vector<float> params, int expected){
+    // takes a string, vector, and expected number of args, fills vector with params
     // if there are more params than expected, ignores the extra params
     //     and issues warning to stderr
-    // EX: "0 4 -1 3", 4 => [0, 4, -1, 3]
-    // EX: "1 2 3 4 5", 3 => [1, 2, 3]
+    params.reserve(expected);
     stringstream ss(line);
     string param;
-    int i = 0;
-    while (getline(ss, param, ' ')){
-        params[i] = stoi(param);
+    unsigned int i = 0;
+    while (getline(ss, param, ' ') and i<expected){
         i++;
+    }
+
+    // ERROR: unexpected behavior
+    if (getline(ss, param, ' ')){
+        cout << param << endl;
+        cerr << "Extra parameters, ignoring" << endl;
     }
 }
 
 // TO IMPLEMENT: individual functions for handling each case
 // each function should take a string listing the parameters
+int case_sph(vector<float> params){
+    float cx=params[0], cy=params[1], cz=params[2], r=params[3];
+    Vector3f center;
+    center << cx, cy, cz;
+    Sphere sph = Sphere(center, r, trans, brdf);
+    return 0;
+}
+
+int case_tri(vector<float> params){
+    float ax=params[0], ay=params[1], az=params[2];
+    float bx=params[3], by=params[4], bz=params[5];
+    float cx=params[6], cy=params[7], cz=params[8];
+    Vector3f a, b, c;
+    a << ax, ay, az;
+    b << bx, by, bz;
+    c << cx, cy, cz;
+    return 0;
+} 
+
 int case_obj(string file_name){
-    // handles the obj case.
     // takes a file_name. Returns positive if successful, negative if not
     file_name = file_name.substr(1, file_name.length()-2); //remove quotes
-    cout << file_name << endl;
     fstream input_file (file_name.c_str());
     if (input_file.is_open()){
-        cout << "Inside case_obj..." << endl;
         string line;
 
         while (getline(input_file, line)){
@@ -60,7 +87,9 @@ int case_obj(string file_name){
             iss >> defn >> args[0] >> args[1] >> args[2];
 
             // cannot handle comments!
-            if (defn == 'v' or defn == 'f'){
+            if (line[0] == '#'){
+            }
+            else if (defn == 'v' or defn == 'f'){
                 Vector3f vertex;
                 vertex << args[0], args[1], args[2];
             }
@@ -72,6 +101,42 @@ int case_obj(string file_name){
     return -1;
 }
 
+int case_ltp(vector<float> params){
+    float px=params[0], py=params[1], pz=params[2];
+    float r=params[3], g=params[4], b=params[5], falloff = params[6];
+    // Double check vector initializes to 0
+    return 0;
+}
+
+int case_ltd(vector<float> params){
+    float dx=params[0], dy=params[1], dz=params[2];
+    float r=params[3], g=params[4], b=params[5];
+    return 0;
+}
+
+int case_mat(vector<float> params){
+    float kar=params[0], kag=params[1], kab=params[2];
+    float kdr=params[3], kdg=params[4], kdb=params[5];
+    float ksr=params[6], ksg=params[7], ksb=params[8], ksp=params[9];
+    float krr=params[10], krg=params[11], krb=params[12];
+    return 0;
+}
+
+int case_xft(vector<float> params){
+    float tx=params[0], ty=params[1], tz=params[2];
+    return 0;
+}
+
+int case_xfr(vector<float> params){
+    float rx=params[0], ry=params[1], rz=params[2];
+    return 0;
+}
+
+int case_xfs(vector<float> params){
+    float sx=params[0], sy=params[1], rz=params[2];
+    return 0;
+}
+    
 int handle_cases(string line){
     // Handles each line in the input file
     // if succesfully handled, returns positive int. Else negative int
@@ -81,32 +146,42 @@ int handle_cases(string line){
     }
     string op = line.substr(0,3);
     string args = line.substr(4);
+    vector<float> params;
 
     // switch cannot take string, so hash it to enum
     switch (hash_it(op)) {
         case cam:
+            get_params(args, params, 15);
             return 0;
         case sph:
-            int params[4];
-            get_params(args, params);
+            get_params(args, params, 4);
+            case_sph(params);
             return 0;
         case tri:
+            get_params(args, params, 9);
             return 0;
         case obj:
             return case_obj(args);
         case ltp:
+            get_params(args, params, 7);
             return 0;
         case ltd:
+            get_params(args, params, 6);
             return 0;
         case lta:
+            get_params(args, params, 3);
             return 0;
         case mat:
+            get_params(args, params, 13);
             return 0;
         case xft:
+            get_params(args, params, 3);
             return 0;
         case xfr:
+            get_params(args, params, 3);
             return 0;
         case xfs:
+            get_params(args, params, 3);
             return 0;
         default:
             return -1;
@@ -119,7 +194,6 @@ int main()
     string file_name;
     cout << "Specify input file: ";
     cin >> file_name;
-    cout << "The value you entered is " << file_name << endl;
 
     fstream input_file (file_name.c_str());
     if (input_file.is_open()){
